ff6cd83c0f6fe4681678a4a9ba2ed9ea6cda52f2
Mahesh Sirikonda <mahesh.sirkonda@gmail.com>
2021-02-02 20:45:00 +0100

    first project check-in

---
+++ src/main/java/com/sirikonda/test/springboottestpractice/SpringBootTestPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.sirikonda.test.springboottestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestPracticeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootTestPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirikonda/test/springboottestpractice/SpringBootTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirikonda.test.springboottestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
